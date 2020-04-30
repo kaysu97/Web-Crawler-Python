e31e2f73be9278f88f4c77246a32f88345116701
KaySu <kay.su@ecloudvalley.com>
2020-04-30 11:16:02 +0800

    add comment

--- crawler-test.py
+++ crawler-test.py
@@ -18,39 +18,41 @@ import base64
 # from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 options = Options()
+# Use Selenium without opening the browser
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')  # Last I checked this was necessary.
-# scrape web without opening a browser
 driver = webdriver.Chrome(options=options)
 # driver = webdriver.Chrome()
-web_url = "https://rent.591.com.tw/?kind=0&region" #前往這個網址
+web_url = "https://rent.591.com.tw/?kind=0&region" #Go to this URL
 region_li= ['1','3']
 data_dict={}
 # driver.find_element_by_xpath('//dd[contains(text(),"台北市"]').click()
 # driver.find_element_by_link_text('2').click()
 
-# price_value = '25,000 月/元'
-# [int(s) for s in price_value.replace(',','').split() if s.isdigit()]
 app = Flask(__name__)
 
 def main():
-    for region in region_li:
-        driver.get(f"{web_url}={region}") #前往這個網址
+    for region in region_li: # Run the region you want
+        driver.get(f"{web_url}={region}") # Go to this website
         time.sleep(3)
-        soup = bs(driver.page_source,'lxml') 
-        page_li = soup.find_all('a', attrs={'class': 'pageNum-form'}) 
-        for page in page_li:
+        soup = bs(driver.page_source,'lxml') # Get the web front-end code
+        page_li = soup.find_all('a', attrs={'class': 'pageNum-form'}) # Find all pages
+        for page_no in range(int(page_li[-1].text)): # Run page from 1 to last
+            page = page_no+1
             if page != 1:
-                driver.find_element_by_link_text(f"{page}").click()
+                driver.find_element_by_link_text(f"{page}").click() # Click the page text
                 time.sleep(3)
                 soup = bs(driver.page_source,'lxml') 
+            # Find all rental information list on this page.
             rent_li = soup.find_all('li', attrs={'class': 'pull-left infoContent'}) 
-            for rent_info in rent_li:
+            for rent_info in rent_li: # Rental detail URL
+                # Get the detail page html label
                 soup_detail = request_detail(rent_info)
-                # Find rent price
+                # Find and process rental price, house and user info
                 price_string(soup_detail)
                 house_info(soup_detail)
                 other_info(soup_detail)
+                # Insert data to mongodb
                 insert_mongodb(data_dict)
 
 def request_detail(rent_info):
@@ -107,15 +109,15 @@ def string_dealer(mix_string):
     return ok_string
 
 def strQ2B(ustring):
-    """把字串全形轉半形"""
+    # Transform fullwidth to halfwidth
     ss = []
     for s in ustring:
         rstring = ""
         for uchar in s:
             inside_code = ord(uchar)
-            if inside_code == 12288:  # 全形空格直接轉換
+            if inside_code == 12288:  # Fullwidth blank space transformation
                 inside_code = 32
-            elif (inside_code >= 65281 and inside_code <= 65374):  # 全形字元（除空格）根據關係轉化
+            elif (inside_code >= 65281 and inside_code <= 65374): 
                 inside_code -= 65248
             rstring += chr(inside_code)
         ss.append(rstring)
